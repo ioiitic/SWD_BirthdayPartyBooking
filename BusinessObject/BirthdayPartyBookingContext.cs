898f162b3134eb9eddc10ad29a1e200c65407853
ioiitic <khoitranminh592003@gmail.com>
2024-03-18 15:39:22 +0700

    Add ConnectionString

--- BusinessObject/BirthdayPartyBookingContext.cs
+++ BusinessObject/BirthdayPartyBookingContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -26,11 +28,12 @@ namespace BusinessObject
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=(local);database=BirthdayPartyBooking;uid=sa;password=12345;");
-            }
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            string test = configuration.GetConnectionString("MyDB");
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyDB"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
